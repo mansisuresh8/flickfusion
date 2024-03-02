d142fa6988c1aa7e4487514effdb828045977aa9
mansisuresh8 <mansijumde0@gmail.com>
2024-03-02 20:06:58 +0530

    update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,23 @@
-import axios from "axios";
+import axios, { Axios } from "axios";
 
-const BASE_URL="https://api.themoviedb.org/3"
+const BASE_URL="https://api.themoviedb.org/3";
 
 const TMDB_TOKEN = import.meta.env.VITE_API_TMDB_TOKEN;
+
+const headers = {
+    Authorization:"bearer " + TMDB_TOKEN,
+};
+
+export const fetchDataFromApi = async (url, params) =>
+{
+    try{
+        const {data} = await axios.get(BASE_URL + url, {
+            headers,
+            params
+        })
+        return data;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+}
